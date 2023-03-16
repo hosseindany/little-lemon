910841801fbeb1956b44eb0c7edfe01f09b51c24
unknown <hosseindany@gmail.com>
2023-03-17 01:13:09 +0330

    some basic modifications up to meta tags

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,11 +1,25 @@
 
 import './App.css';
+import Header from './Header';
+import Nav from './Nav';
+import Main from './Main';
+import Footer from './Footer';
+import MetaTags from 'react-meta-tags';
 
 function App() {
   return (
-    <div className="App">
-Homepage
-    </div>
+    <>
+    <title>Page 1</title>
+    <MetaTags>
+    <meta id="meta-description" name="description" content="Some description." />
+    <meta id="og-title" property="og:title" content="MyApp" />
+    <meta id="og-image" property="og:image" content="path/to/image.jpg" />
+    </MetaTags>
+      <Header></Header>
+      <Nav></Nav>
+      <Main></Main>
+      <Footer></Footer>
+    </>
   );
 }
 
---
+++ little-lemon/src/Header.js
@@ -0,0 +1,8 @@
+function Header(){
+    return(
+        <header>
+            <a href="/"><img src='logo.ong' alt="little lemon"/></a>
+        </header>
+    )
+}
+export default Header;
